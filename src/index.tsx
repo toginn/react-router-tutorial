ad953505f7b9ab690c52461a2eeffe7a35655279
toginn <89682463+toginn@users.noreply.github.com>
2022-04-01 15:29:53 +0900

    Add: router links

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
