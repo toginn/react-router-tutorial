b22189761a8914c5f7e04f64d17e494633b1eb25
toginn <89682463+toginn@users.noreply.github.com>
2022-04-01 17:45:28 +0900

    Add: invoice details

--- src/routes/invoice.tsx
+++ src/routes/invoice.tsx
@@ -1,8 +1,26 @@
 import { useParams } from "react-router-dom";
+import { getInvoice } from "../data";
 
 const Invoice = () => {
   let params = useParams();
-  return <h2>Invoice: {params.invoiceId}</h2>;
+  let invoice = params.invoiceId
+    ? getInvoice(parseInt(params.invoiceId, 10))
+    : undefined;
+  if (invoice) {
+    return (
+      <main style={{ padding: "1rem" }}>
+        <h2>Invoice: {params.invoiceId}</h2>
+        <p>
+          {invoice.name}: {invoice.number}
+        </p>
+      </main>
+    );
+  }
+  return (
+    <main style={{ padding: "1rem" }}>
+      <p>Invoice not found.</p>
+    </main>
+  );
 };
 
 export default Invoice;
